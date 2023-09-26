11a8ee9d627949c8131a62b18311aedcd7690d26
Lindsay <hoyt.lindsay@siu.edu>
2023-09-26 15:11:17 -0500

    Implemented Addition Feature

--- calculator.c
+++ calculator.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int add(int x, int y);
+
 int main()
 {
+	int result;
 	
+	result = add (3, 4);
+	printf("%d", result);
+	return 0;
+}
+
+int add(int x, int y)
+{
+	return x + y;
 }
