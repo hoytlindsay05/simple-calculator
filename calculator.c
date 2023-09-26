6c87f085ada6ce16f08971561cd7179a9da81722
Lindsay <hoyt.lindsay@siu.edu>
2023-09-26 16:11:56 -0500

    Added comments and documentation

--- calculator.c
+++ calculator.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
+//Funcion declarations
 int add(int x, int y);
 int sub(int x, int y);
 int mult(int x, int y);
@@ -8,16 +9,21 @@ float div(int x, int y);
 
 int main()
 {
+	//Declare variables for input and results
 	int int_result, input, num1, num2;
 	float div_result;
 	
+	//Query the user for which opperation to use
 	printf("Select Operation:\n");
 	printf("1 for addition\n2 for subtraction\n3 for multiplication\n4 for division\n");
 	scanf("%d", &input);
 	
+	//Switch case for opperations
 	switch(input)
 	{
+		//Addition case
 		case 1:
+			//Query user for first two values and check for valid input
 			printf("Enter first value\n");
 			if (scanf("%d", &num1) != 1)
 			{
@@ -34,7 +40,9 @@ int main()
 			int_result = add(num1, num2);
 			printf("%d\n", int_result);
 			break;
+		//Subtraction case
 		case 2:
+			//Query user for first two values and check for valid input
 			printf("Enter first value\n");
 			if (scanf("%d", &num1) != 1)
 			{
@@ -51,7 +59,9 @@ int main()
 			int_result = sub(num1, num2);
 			printf("%d\n", int_result);
 			break;
+		//Multiplication case
 		case 3:
+			//Query user for first two values and check for valid input
 			printf("Enter first value\n");
 			if (scanf("%d", &num1) != 1)
 			{
@@ -68,7 +78,9 @@ int main()
 			int_result = mult(num1, num2);
 			printf("%d\n", int_result);
 			break;
+		//Division case
 		case 4:
+			//Query user for first two values and check for valid input
 			printf("Enter first value\n");
 			if (scanf("%d", &num1) != 1)
 			{
@@ -82,6 +94,7 @@ int main()
 				break;
 			}
 			
+			//Check for division by 0 error
 			if (num2 == 0)
 			{
 				printf("Error: Division by 0\n");
@@ -91,27 +104,32 @@ int main()
 			div_result = div(num1, num2);
 			printf("%.2f\n", div_result);
 			break;
+		//User did not enter 1, 2, 3, or 4
 		default:
 			printf("Invalid input\n");
 	}
 	return 0;
 }
 
+//Addition funciton
 int add(int x, int y)
 {
 	return x + y;
 }
 
+//Subtraction function
 int sub(int x, int y)
 {
 	return x - y;
 }
 
+//Multiplication function
 int mult(int x, int y)
 {
 	return x * y;
 }
 
+//Division function
 float div(int x, int y)
 {
 	return x / y;
