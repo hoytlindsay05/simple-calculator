fe2361838fbc9bc9c0bf5b8ba8f4043d4bc2c537
Lindsay <hoyt.lindsay@siu.edu>
2023-09-26 15:42:49 -0500

    Implemented division feature

--- calculator.c
+++ calculator.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
 int add(int x, int y);
 int sub(int x, int y);
 int mult(int x, int y);
+float div(int x, int y);
 
 int main()
 {
-	int result;
+	int int_result;
+	float div_result;
 	
-	result = add(3, 4);
-	printf("%d\n", result);
+	int_result = add(3, 4);
+	printf("%d\n", int_result);
 	
-	result = sub(4, 3);
-	printf("%d\n", result);
+	int_result = sub(4, 3);
+	printf("%d\n", int_result);
 	
-	result = mult(3, 4);
-	printf("%d\n", result);
+	int_result = mult(3, 4);
+	printf("%d\n", int_result);
+	
+	div_result = div(4, 2);
+	printf("%.2f\n", div_result);
 	return 0;
 }
 
@@ -32,4 +38,9 @@ int sub(int x, int y)
 int mult(int x, int y)
 {
 	return x * y;
+}
+
+float div(int x, int y)
+{
+	return x / y;
 }
