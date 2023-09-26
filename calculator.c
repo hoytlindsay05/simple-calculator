0ae9b6daab35289e46a05a00841e1cad439d449d
Lindsay <hoyt.lindsay@siu.edu>
2023-09-26 15:14:25 -0500

    Implemented subtraction feature

--- calculator.c
+++ calculator.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 
 int add(int x, int y);
+int sub(int x, int y);
 
 int main()
 {
 	int result;
 	
-	result = add (3, 4);
-	printf("%d", result);
+	result = add(3, 4);
+	printf("%d\n", result);
+	
+	result = sub(4, 3);
+	printf("%d\n", result);
 	return 0;
 }
 
 int add(int x, int y)
 {
 	return x + y;
+}
+
+int sub(int x, int y)
+{
+	return x - y;
 }
